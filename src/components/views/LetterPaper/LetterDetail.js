fa586ec54f30f8a7bdbaff2617ffa807811db075
MinJi1999 <didtjdrla@gmail.com>
2021-06-16 02:06:45 +0900

    fix: UI 및 창닫을 때 실행되는 함수 수정

--- src/components/views/LetterPaper/LetterDetail.js
+++ src/components/views/LetterPaper/LetterDetail.js
@@ -5,56 +5,51 @@ import { useDispatch } from 'react-redux';
 import { addToCart } from '../../../_actions/user_actions';
 import apiClient from "../../apiClient";
 
-function LetterDetail(props) {
-    const [Product, setProduct] = React.useState({});
-    const [Modal, setModal] = React.useState(true);
-
-    React.useEffect(() => {
-        const productId = props.match.params.productId;
+function LetterDetail({props, open, close}) {
+    const [product, setProduct] = React.useState({});
+    if(props){
+        const productId = props;
         apiClient.get(`/api/product/products_by_id?id=${productId}&type=single`)
             .then(res => {
                 setProduct(res.data[0])
             })
             .catch(err => alert('상세 정보 가져오기를 실패했어요(letterDetail). Error : ', err))
-    }, [])    
-
-    const showPaper = () => {
-        setModal(false);
     }
     const dispatch = useDispatch();
     const clickHandler = () => {
-        dispatch(addToCart(Product._id))
+        dispatch(addToCart(product._id))
         .then(alert("상품을 선택하셨습니다!"))
-        .then(showPaper)
     }
-    if(!Modal){
-        props.history.push("/product/letterpaper")
-    }return(
+
+    return(
         <>
+        {
+            open
+            ?
             <div className="black-bg">
-                    <div className="button-cart" onClick={clickHandler}>
-                        장바구니에 담기
-                        <div className="button-cart-text">C A R T</div>
-                    </div>
-                <div className="modal-wrap">
-                    <div className="modal-close" onClick={() => setModal(false)}> 
-                        창 닫 기
-                    </div>
-                    <div className="detail-box" >
-                        <ProductImage detail={Product} />
-                        <div className="detail-description-container">
-                            <ProductDescription function={showPaper}detail={Product}/>
+                <div className="box-shadow">
+                        <div className="button-cart" onClick={clickHandler}>
+                            장바구니에 담기
+                            <div className="button-cart-text">C A R T</div>
+                        </div>
+                    <div className="modal-wrap">
+                        <div className="modal-close" onClick={close}> 
+                            창 닫 기
+                        </div>
+                        <div className="detail-box" >
+                            <ProductImage detail={product} />
+                            <div className="detail-description-container">
+                                <ProductDescription detail={product}/>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
+            :
+            null
+        }
         </>
     )
-        
-    
-    
-        
-    
 }
 
 export default LetterDetail
